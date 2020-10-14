f64c6cbd92966dd5a5eef1b4799136d06984a079
Adam Hare <adam.hare@zoomi.ai>
2020-10-14 12:09:01 -0400

    Updated results

--- survey_app/utils.py
+++ survey_app/utils.py
@@ -11,27 +11,28 @@ def get_response_results(include_never=True):
     for f in files:
         with open(f, 'r') as resp_file:
             resp = json.load(resp_file)
-        # Top-level questions
-        summary["num_responses"] += 1
-        summary["major"][resp["major"]] += 1
-        summary["frequency"][resp["frequency"]] += 1
-        summary["adoption"][resp["adoption"]] += 1
-        summary["level"][resp["level"]] += 1
-        if not include_never and resp["frequency"] == "A":
-            continue
+        if resp["survey_type"] == "choose-3":
+            # Top-level questions
+            summary["num_responses"] += 1
+            summary["major"][resp["major"]] += 1
+            summary["frequency"][resp["frequency"]] += 1
+            summary["adoption"][resp["adoption"]] += 1
+            summary["level"][resp["level"]] += 1
+            if not include_never and resp["frequency"] == "A":
+                continue
 
-        # Responses for each comparison
-        for r in resp["query_responses"]:
-            if "weighted_set_cover" in r["option_to_model"].values():
-                summary["num_answered_weighted"] += 1
-                for inc in r["result"]:
-                    model = r["option_to_model"][inc]
-                    summary[model]["included_count_weighted"] += 1
-            else:
-                summary["num_answered_unweighted"] += 1
-                for inc in r["result"]:
-                    model = r["option_to_model"][inc]
-                    summary[model]["included_count_unweighted"] += 1
+            # Responses for each comparison
+            for r in resp["query_responses"]:
+                if "weighted_set_cover" in r["option_to_model"].values():
+                    summary["num_answered_weighted"] += 1
+                    for inc in r["result"]:
+                        model = r["option_to_model"][inc]
+                        summary[model]["included_count_weighted"] += 1
+                else:
+                    summary["num_answered_unweighted"] += 1
+                    for inc in r["result"]:
+                        model = r["option_to_model"][inc]
+                        summary[model]["included_count_unweighted"] += 1
                 
 
     # Get averages and win percentages
